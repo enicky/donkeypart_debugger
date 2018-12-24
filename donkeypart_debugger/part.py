6f2bbff11711d3f53a0c9dca4a791cfaa57a1b79
nicholase <enicky80@gmail.com>
2018-12-24 10:31:33 +0100

    added usermode

--- donkeypart_debugger/part.py
+++ donkeypart_debugger/part.py
@@ -2,7 +2,7 @@ class DonkeypartDebugger:
     def __init__(self):
         print("init debugger")
 
-    def run_threaded(self, image_array=None, steering=0, throttle=0, recording=False):
+    def run_threaded(self, image_array=None, steering=0, throttle=0,usermode='user',recording=False):
         if image_array is not None:
             print("Sizeof ImageArryay : " + len(image_array))
         else:
@@ -10,9 +10,9 @@ class DonkeypartDebugger:
         print("Steering : " + str(steering))
         print("Throttle : " + str(throttle))
         print("Recording: " + str(recording))
-        return image_array, steering, throttle, recording
-    
-    def run(self,  image_array=None, steering=0, throttle=0, recording=False):
+        return image_array, steering, throttle, usermode, recording
+
+    def run(self,  image_array=None, steering=0, throttle=0, usermode='user',recording=False):
         if image_array is not None:
             print("Sizeof ImageArryay : " + len(image_array))
         else:
@@ -20,7 +20,8 @@ class DonkeypartDebugger:
         print("Steering : " + str(steering))
         print("Throttle : " + str(throttle))
         print("Recording: " + str(recording))
-        return image_array, steering, throttle, recording
+        print("usermode : " + usermode)
+        return image_array, steering, throttle, usermode, recording
 
 
 if __name__ == "__main__":
